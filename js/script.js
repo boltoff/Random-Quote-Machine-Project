377e9e7d7b772abc93bd95a22f3f94ab4aaa3a7b
Vladislav <flowvlad@gmail.com>
2016-12-18 17:14:39 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("button").click(function () {
+    var html = '<i id="loading" class="fa fa-spinner fa-spin fa-3x"></i>';
+    $("#quoteplace").html(html);
+    $.getJSON("https://andruxnet-random-famous-quotes.p.mashape.com/endpoint?mashape-key=mFRfHTOUp0mshWC5yVHhoPWdVYOvp1mULMMjsn7Dglyj7jydgf&cat=famous",
+        function (result) {
+            html = '<p>“' + result.quote + '”</p>';
+            html += '<p id="autor">- ' + result.author + '</p>';
+            $("#quoteplace").html(html);
+        });
+});
